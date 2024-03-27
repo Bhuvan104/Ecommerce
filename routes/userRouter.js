bb25735fc036cdbf034d1e67037572b44e6f9c76
bhuvanbk <bhuvan.kumar@learnship.com>
2024-03-28 00:54:15 +0530

    Added some description related to routers

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const authMiddleware = require('../middleware/authMiddleware');
+//getUsers_add_orders
 router.get('/addresses-orders',  userController.getUsers_add_orders);
+// getUsers_add_orders_product
 router.get('/addresses-orders-product',  userController.getUsers_add_orders_product);
+//createUserWithOrdersAndProducts
 router.post('/addresses-orders-product',  userController.createUserWithOrdersAndProducts)
+//createAddressForUser
 router.post('/:userId/address', userController.createAddressForUser);
 // Create a new user
 router.post('/',authMiddleware, userController.createUser);
